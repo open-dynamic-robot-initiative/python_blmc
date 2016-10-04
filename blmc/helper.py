ab38832991c404b0cdbca67f596429fd24aaa946
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2016-10-04 15:01:47 +0200

    forgot to add file in last commit

---
+++ blmc/helper.py
@@ -0,0 +1,13 @@
+"""Some helper functions."""
+import sys
+import time
+
+def get_time():
+    if sys.platform == 'win32':
+        # On Windows, the best timer is time.clock
+        return time.clock()
+    else:
+        # On Linux, clock does not return wall time but CPU time. use time.time
+        # instead (which has much higher accuracy for linux than for windows,
+        # so it is fine).
+        return time.time()
